4e6a9df274c438dcd3f6930c1917ba9c6f59bcaf
Meesohy <124698885+Meesohy@users.noreply.github.com>
2023-03-06 19:06:42 -0500

    Update program.py

--- program.py
+++ program.py
@@ -1,8 +1,7 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction("Hello World!"):
+def myFunction():
   # My Function
-print("Hello World!")
-               
+print("Hello World!")            
 return "Hello World!"
